5f88d0870266ddd73c29e1e3b877fe52b884aca2
Cesar Cortez <77755904+cesar-cortez5@users.noreply.github.com>
2021-02-10 08:49:20 -0600

    Update lab_219.py

--- homework_1/lab_219.py
+++ homework_1/lab_219.py
@@ -1,3 +1,6 @@
+#Cesar Cortez
+#PSID = 1836168
+
 lemon_juice = float(input("Enter amount of lemon juice (in cups):\n"))
 water = float(input("Enter amount of water (in cups):\n"))
 agave_nectar = float(input("Enter amount of agave nectar (in cups):\n"))
