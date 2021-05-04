14f0f813e3756c3f1628d44b23d8ebe951c6b9fa
Cesar Cortez <cctez36@gmail.com>
2021-05-04 18:10:37 -0500

    lab12.7

---
+++ homework_4/lab_127.py
@@ -0,0 +1,19 @@
+def get_age():
+    age = int(input())
+    if age < 18 or age > 75:
+        raise ValueError("Invalid age.")
+    return age
+
+
+def fat_burning_heart_rate(age):
+    heart_rate = (220 - 35) * 0.7
+    return heart_rate
+
+if __name__ == "__main__":
+    try:
+        age = get_age()
+        bpm = fat_burning_heart_rate(age)
+        print(f"Fat burning heart rate for a {age} year-old: {bpm} bpm")
+
+    except ValueError:
+        print("Invalid age.\nCould not calculate heart rate info.")
