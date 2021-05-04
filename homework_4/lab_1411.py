ff3824c5063a68ec31e1ee6ee7f0c14a4cdcdb48
Cesar Cortez <cctez36@gmail.com>
2021-05-04 18:36:12 -0500

    lab1411

---
+++ homework_4/lab_1411.py
@@ -0,0 +1,17 @@
+def selection_sort_descend_trace(numbers):
+    for i in range(len(numbers) - 1):
+        index_largest = i
+        for j in range(i + 1, len(numbers)):
+            if numbers[j] > numbers[index_largest]:
+                index_largest = j
+        numbers[i], numbers[index_largest] = numbers[index_largest], numbers[i]
+
+        print(' '.join([str(x) for x in numbers]),'')
+        
+    return numbers
+
+
+if __name__ == '__main__':
+    numbers = [int(x) for x in input().split()]
+
+    selection_sort_descend_trace(numbers)
