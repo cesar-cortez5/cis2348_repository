4576f044c9ebc27931754f45130566d570d8e99d
Cesar Cortez <cctez36@gmail.com>
2021-05-06 16:02:49 -0500

    formatting

--- FinalProjectPart1/FinalProjectInput copy.py
+++ FinalProjectPart1/FinalProjectInput copy.py
@@ -1,13 +1,15 @@
-#Cesar Cortez
+# Cesar Cortez
 #PSID - 1836168
 
 
 import csv
 from datetime import datetime
 
-#Defining the class named Item
+# Defining the class named Item
+
+
 class Item:
-    #Initializing the class with the attributes manufuctured, price, and service_date, which are all list 
+    # Initializing the class with the attributes manufuctured, price, and service_date, which are all list
     def __init__(self, manufacturer, price, service_date):
         self.manufacturer = manufacturer
         self.price = price
@@ -20,17 +22,15 @@ class Item:
             for j in self.price:
                 for k in self.service_date:
                     if i[0] == k[0] and i[0] == k[0] and i[0] == j[0]:
-                        full_inventory_list.append([i[0], i[1], i[2], j[1], k[1], i[3]])
+                        full_inventory_list.append(
+                            [i[0], i[1], i[2], j[1], k[1], i[3]])
 
         for i in full_inventory_list:
             if i[5] == "damaged":
                 damaged_list.append(i)
 
-        
-
-
         item_in_inv = False
-        #Finished checking for manufacturer and item type. Need to add to check for different item types/manufactures in one string (lenovo apple computer, dell phone laptop,)
+        # Finished checking for manufacturer and item type. Need to add to check for different item types/manufactures in one string (lenovo apple computer, dell phone laptop,)
         for i in full_inventory_list:
             other_items = []
             manufacturer = i[1].lower()
@@ -45,18 +45,14 @@ class Item:
                 customer_price = i[3]
                 item_in_inv = True
 
-
-
-
         if item_in_inv:
-            print(f"Your item is: {customer_id}, {customer_manufacturer}, {customer_item_type}, {customer_price}")
+            print(
+                f"Your item is: {customer_id}, {customer_manufacturer}, {customer_item_type}, {customer_price}")
         else:
             print(f"No such item in inventory")
 
 
-
-        
-#Main function that opens each file, takes each row from each CSV file, and imports it to a list
+# Main function that opens each file, takes each row from each CSV file, and imports it to a list
 def file_opener():
     manufacturer_list = []
     with open('ManufacturerList.csv', 'r') as csv_file:
@@ -79,15 +75,11 @@ def file_opener():
     return manufacturer_list, price_list, service_dates_list
 
 
-
-
-#Main function
+# Main function
 if __name__ == "__main__":
 
-
-
     manufacturer_list, price_list, service_dates_list = file_opener()
-    #Creating an instance of an Item known as Item1
+    # Creating an instance of an Item known as Item1
     Item1 = Item(manufacturer_list, price_list, service_dates_list)
 
     query = ""
@@ -95,15 +87,3 @@ if __name__ == "__main__":
     while query != "q":
         query = input("Please enter the item type and manufacturer\n")
         Item1.customer_query(query)
-
-
-
-
-
-
-                        
-                    
-
-
-
-
